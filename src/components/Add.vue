eb7222b4a0e9b1cc07781ab523dbd6c0a32b61c8
JINDAL <ankit.jindal@thermofisher.com>
2022-10-25 11:14:19 +0530

    adding add and update page

---
+++ src/components/Add.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <h1>Hi, Welcome On Add Restaurant Page</h1>
+</template>
+<script>
+import Header from './Header.vue'
+export default {
+  name: "Add",
+  components: {
+    Header
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
+  },
+};
+</script>
+<style>
+
+</style>
---
+++ src/components/Update.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <h1>Hi, Welcome On update Restaurant Page</h1>
+</template>
+<script>
+import Header from './Header.vue'
+export default {
+  name: "Update",
+  components: {
+    Header
+  },
+  mounted() {
+    let user = localStorage.getItem("user-info");
+    if (!user) {
+      this.$router.push({ name: "SignUp" });
+    }
+  },
+};
+</script>
+<style>
+
+</style>
